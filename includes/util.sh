a830d8e1261649b9f6004eccc133878ea98720c2
Jerred Shepherd <ShepherdJerred@gmail.com>
2016-11-16 20:30:48 -0600

    Update util.sh

--- includes/util.sh
+++ includes/util.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 
 generateRandomName() {
-    shuf -n 2 includes/words.txt
-    echo "$name"
+	adverb="$(shuf -n 1 includes/adverbs.txt)"
+	adjective="$(shuf -n 1 includes/adjectives.txt)"
+	noun="$(shuf -n 1 includes/nouns.txt)"
+	echo "$adverb$adjective$noun"
 }
